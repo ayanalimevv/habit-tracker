d5aa58612f0800dcf8ca32dd9d8608f243e0e75c
alpha2207_dev <ayan787ali.mev@gmail.com>
2023-12-17 03:09:47 +0530

    updated box to show modal on click

--- src/app/components/BrownMonthBox.tsx
+++ src/app/components/BrownMonthBox.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { useHabitModal } from "./HabitModalContext";
 
-const BrownMonthBox = () => {
+const BrownMonthBox = ({
+  habitId,
+  year,
+  month,
+  date,
+}: {
+  habitId: string;
+  year: number;
+  month: number;
+  date: number;
+}) => {
+  const { openHabitModal } = useHabitModal();
   return (
-      <div className="h-4 w-4 bg-[#606060] rounded hover:cursor-pointer hover:scale-110"></div>
+    <div
+      onClick={() => openHabitModal(habitId, { year, month, date })}
+      className="h-4 w-4 bg-[#606060] rounded hover:cursor-pointer hover:scale-110"
+    ></div>
   );
 };
 
--- src/app/components/GreenMonthBox.tsx
+++ src/app/components/GreenMonthBox.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { openModalById } from "../helpers/openModalById";
+import { useHabitModal } from "./HabitModalContext";
 
-const GreenMonthBox = () => {
+const GreenMonthBox = ({
+  habitId,
+  year,
+  month,
+  date,
+}: {
+  habitId: string;
+  year: number;
+  month: number;
+  date: number;
+}) => {
+  const { openHabitModal } = useHabitModal();
   return (
-    <div className="h-4 w-4 bg-[#15fd00b8] rounded hover:cursor-pointer hover:opacity-80 hover:scale-110"></div>
+    <div
+      onClick={() => openHabitModal(habitId, { year, month, date })}
+      className="h-4 w-4 bg-[#15fd00b8] rounded hover:cursor-pointer hover:opacity-80 hover:scale-110"
+    ></div>
   );
 };
 
