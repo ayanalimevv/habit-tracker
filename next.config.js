6f73a0811b79ef54be3c9ad3cf098d35118bde1f
alpha2207_dev <ayan787ali.mev@gmail.com>
2023-12-17 03:09:12 +0530

    added config for popup

--- next.config.js
+++ next.config.js
@@ -6,7 +6,21 @@ const nextConfig = {
       "lh3.googleusercontent.com",
       "daisyui.com",
       "firebasestorage.googleapis.com",
+      "placehold.co",
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin allow-popups",
+          },
+        ],
+      },
+    ];
   },
 };
 
